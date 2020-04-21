868ac7d6a0e3eacd46a89985a35b865ff291bddd
alexander-kucheryavenko <aiongame85@gmail.com>
2020-04-21 13:46:54 +0300

    first

---
+++ config/mock.keys.js
@@ -0,0 +1,8 @@
+module.exports = {
+  mongoURI: 'mongodb://localhost/user',
+  skeys: 'secret_node',
+  jwt: 'dev-jwt',
+  emailFrom: 'mchaw0786@gmail.com',
+  gmailPassword: 'H8cHPMumctTSoZ3U',
+  appUrl: 'btl.com'
+};
