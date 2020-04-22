6f8780db259745ecf4b69a875a941c72e4c0246a
alexander-kucheryavenko <aiongame85@gmail.com>
2020-04-22 11:08:03 +0300

    created new rout for update password & verify token

--- controllers/auth.js
+++ controllers/auth.js
@@ -5,6 +5,7 @@ const User = require('../models/user');
 const keys = require('../config/keys');
 const errorHandler = require('../routes/utils/errorHandler');
 const MailService = require('../services/mail.service');
+const saltWork = 10;
 
 module.exports.login = async (req, res) => {
   const candidate = await User.findOne({
@@ -71,5 +72,91 @@ module.exports.register = async (req, res) => {
     } catch (e) {
       errorHandler(res, e)
     }
+  }
+};
+
+module.exports.verify = async (req, res) => {
+  try {
+    const decoded = await jwt.verify(req.body.token, keys.jwt);
+    const candidate = await User.findOne({
+      email: decoded.email
+    });
+    if (candidate) {
+      res.status(201).json({
+        candidate,
+      })
+    } else {
+      res.status(404).json({
+        message: 'user not found'
+      })
+    }
+  } catch (err) {
+    res.status(401).json({
+      message: 'User un authorize.'
+    })
+  }
+};
+
+module.exports.requestUpdatePassword = async (req, res) => {
+  try {
+    const candidate = await User.findOne({
+      email: req.body.email
+    });
+    if (candidate) {
+      const token = jwt.sign({
+        email: candidate.email,
+        date: new Date()
+      }, keys.jwt, {
+        expiresIn: 60 * 60
+      });
+      const updated = {
+        token
+      };
+      await User.findOneAndUpdate({
+        email: candidate.email,
+      }, {
+        $set: updated
+      });
+      const text = `<div>
+<h4>Do you want to update your password?</h4>
+<p>when you click on this 
+<a href="localhost:3000/updatePassword/token=${token}">link</a>
+ in your account will be reset password ${candidate.email}</p>
+ </div>
+`
+      await MailService.sendSubscribeEmail(candidate.email, text)
+      res.status(201).json({
+        message: 'Message sent'
+      })
+    } else {
+      res.status(404).json({
+        message: 'user not found'
+      })
+    }
+  } catch (err) {
+    res.status(401).json({
+      message: 'User un authorize.'
+    })
+  }
+};
+
+module.exports.changePassword = async (req, res) => {
+  try {
+    const salt = bcrypt.genSaltSync(saltWork);
+    const hash = bcrypt.hashSync(req.body.password, salt);
+    const updated = {
+      password: hash,
+      token:''
+    }
+    await User.findOneAndUpdate({
+      token: req.body.token,
+    }, {
+      $set: updated
+    });
+    res.status(200).json({
+      message: 'password updated successfully'
+    })
+  } catch (e) {
+    errorHandler(res, e)
   }
 };
