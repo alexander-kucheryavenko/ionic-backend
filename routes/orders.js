e89ebe0afd0efee340d9fe21ed4c603ea8137e09
alexander-kucheryavenko <aiongame85@gmail.com>
2020-04-24 16:30:28 +0300

    create new schema orders & path for orders (#6)

---
+++ routes/orders.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const controller = require('../controllers/orders');
+
+const router = express.Router();
+
+// localhost:8000/orders/create
+router.post('/create', controller.create);
+// localhost:8000/orders/get
+router.post('/get', controller.get);
+// localhost:8000/orders/getall
+router.post('/getall', controller.getAll);
+// localhost:8000/orders/update
+router.post('/update', controller.update);
+
+
+module.exports = router;
