264ae272d01ab3ac5893d86db257fcf37d2128db
alexander-kucheryavenko <aiongame85@gmail.com>
2020-04-26 20:11:04 +0300

    update readme

--- config/mock.keys.js
+++ config/mock.keys.js
@@ -2,7 +2,7 @@ module.exports = {
   mongoURI: 'mongodb://localhost/user',
   skeys: 'secret_node',
   jwt: 'dev-jwt',
-  emailFrom: 'mchaw0786@gmail.com',
+  emailFrom: 'mchaw@gmail',
   gmailPassword: 'H8cH',
   appUrl: 'api-ionik.com'
 };
